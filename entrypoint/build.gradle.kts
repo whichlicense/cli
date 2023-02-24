ed04f0574f3e14fc8a9f8920717481934915bbc0
David Greven <opensource@grevend.dev>
2023-02-24 19:44:49 +0100

    Create CLI scaffold
    Signed-off-by: David Greven <opensource@grevend.dev>

---
+++ entrypoint/build.gradle.kts
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2023 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/whichlicense/cli.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("signing")
+}
+
+group = "com.whichlicense.cli"
+version = "0.0.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(19))
+        vendor.set(JvmVendorSpec.GRAAL_VM)
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("info.picocli:picocli:4.7.1")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("--enable-preview")
+    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+    jvmArgs("--enable-preview")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("entrypoint") {
+            artifactId = "entrypoint"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("WhichLicense cli/entrypoint")
+                description.set("The WhichLicense platform CLI entry point.")
+                url.set("https://github.com/whichlicense/cli/entrypoint")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("grevend")
+                        name.set("David Greven")
+                        email.set("david.greven@whichlicense.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/whichlicense/cli.git")
+                    developerConnection.set("scm:git:git@github.com:whichlicense/cli.git")
+                    url.set("https://github.com/whichlicense/cli")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/whichlicense/cli")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+signing {
+    if (project.hasProperty("CI")) {
+        val signingKey = System.getenv("PKG_SIGNING_KEY")
+        val signingPassword = System.getenv("PKG_SIGNING_PW")
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publishing.publications["entrypoint"])
+    }
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
---
+++ entrypoint/src/main/java/com/whichlicense/cli/Entrypoint.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/whichlicense/cli.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.whichlicense.cli;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import static java.lang.System.exit;
+
+/**
+ * The WhichLicense platform CLI base command.
+ *
+ * @author David Greven
+ * @version 0
+ * @since 0.0.0
+ */
+@Command(name = "whichlicense", description = "WhichLicense platform CLI",
+        usageHelpAutoWidth = true, showEndOfOptionsDelimiterInUsageHelp = true,
+        mixinStandardHelpOptions = true, showAtFileInUsageHelp = true, requiredOptionMarker = '*')
+public class Entrypoint implements Runnable {
+    /**
+     * The primary CLI entry point.
+     *
+     * @param args The commands, subcommands, options, flags and arguments supplied to the CLI.
+     * @since 0.0.0
+     */
+    public static void main(String[] args) {
+        exit(new CommandLine(new Entrypoint()).execute(args));
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Coming Soon");
+    }
+}
